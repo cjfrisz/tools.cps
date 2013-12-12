16dca523d40918815526345c8528ffe4b48d13fe
Chris Frisz <chris.frisz@gmail.com>
2013-12-11 22:14:26 -0500

    Fleshing out the accepted language.

--- src/main/clojure/clojure/tools/cps.clj
+++ src/main/clojure/clojure/tools/cps.clj
@@ -1,9 +1,33 @@
 (ns clojure.tools.cps
   (:require [clojure.tools.analyzer :as analyze]))
 
-(def cps-expr? (constantly false))
+(defn cps-expr?
+  [expr]
+  (case (:op expr)
+    :number          true
+    :boolean         true
+    :constant        true  ; quoted values...and set! I guess
+    :var             true
+    :string          true
+    :keyword         true
+    :def             true
+    :fn-expr         true
+    :if              true
+    :do              true
+    :let             true
+    :invoke          true  ; function application & loop
+    :recur           true
+    :throw           true
+    :try             true
+    :monitor-enter   true
+    :monitor-exit    true
+    :instance-method true
+    :new             true
+    :static-method   true  ; primitives, i.e. +
+    false))
 
 (defmacro cps-test
   [expr]
-  (let [analyzed (analyze/macroexpand `(analyze/analyze-form ~expr))]
+  (let [analyzed (analyze/analyze-form (analyze/macroexpand expr))]
+    (clojure.pprint/pprint analyzed)
     (cps-expr? analyzed)))
