8f80049de2b385be7ee7a58665e718786d6fbc33
Chris Frisz <chris.frisz@gmail.com>
2013-12-23 00:17:19 -0500

    Drafting the CPS algorithm for function application.
    Adding support for performing the Danvy-style CPS algorithm for function
    application expressions with zero or more arguments. It differs from the
    one implemented for clojure-tco both in its structure and the fact that
    it actually works for non-trivial expressions, i.e. nested function
    applications (this just goes to show how important test cases are
    and how sloppy I was with some parts of the clojure-tco implementation).
    
    The algorithm proceeds by first building two copies of the sequence
    representing the original function application, one that is simply
    reversed, and the other that is both reversed and has each expression
    trivialized. An expression is trivialized by either a) applying the
    CPS transformation if it is already trivial or b) replacing it with
    a variable that will be used to represent it in a continuation.
    Additionally, the algorithm maintains the current output expression,
    which is initialized to the trivialized application expression in
    the original (non-reversed) order with the initial continuation
    expression inserted appropriately (for tools.cps, this means the
    continuation is the second argument).
    
    Then, then algorithm iterates over the two copies of the reversed
    sequences of expressions in lock step. If the first expression is
    trivial, the output expression is unchanged, and the algorithm iterates
    to the next values for the expression sequences. If the first expression
    is serious, a new output expression is created by applying the CPS
    transformation to the first expression with a continuation constructed
    with the first item of the trivialized expression sequence as the
    argument and the previous output application expression as the body.
    Using the CPS version of the expression as the new potential output, the
    algorithm iterates over the remaining expressions.
    
    In addition to the new CPS algorithm, some unused code is being removed
    and minor edits to existing code based on previous comments are being
    performed.

--- src/main/clojure/clojure/tools/cps.clj
+++ src/main/clojure/clojure/tools/cps.clj
@@ -12,7 +12,9 @@
      :string
      :keyword
      :fn-expr
-     :local-binding-expr)
+     :local-binding
+     :local-binding-expr
+     :var)
     ,true
     :def (trivial-expr? (:init expr))
     :if (every? trivial-expr? ((juxt :test :then :else) expr))
@@ -23,54 +25,72 @@
     :static-method (every? trivial-expr? (:args expr))
     false))
     
-(def serious-expr? (complement trivial-expr?))
+(defn update-in+
+  [coll key* f & arg*]
+  (letfn [($update-in+ [coll key*]
+            (if-let [key (first key*)]
+              (let [key* (next key*)]
+                (if (identical? key clojure.core/first)
+                    (conj (next coll) ($update-in+ (first coll) key*))
+                    (assoc coll key ($update-in+ (get coll key) key*))))
+              (apply f coll arg*)))]
+    ($update-in+ coll key*)))
 
 (defn make-fresh-var
   ([] (make-fresh-var 'g))
   ([name] {:op :local-binding,
            ;; NB: consider passing in programs ns info
-           ;; NB: the above comment suggests a *ton* of work for error-
-           ;; NB: reporting consistency
+           ;; NB: this suggests a *ton* of work for error-reporting consistency
            :env {:locals {} :ns {:name 'clojure.tools.cps}},
            :sym (gensym name),
            :tag nil,
            :init nil}))
 
-;; NB: there's a nasty number of calls to apply in here.
-;; NB: might letfn-bind the main part of the work so the rest
-;; NB: args are only passed in once
-(defn update-in+
-  [coll key* f & arg*]
-    (if-let [key (first key*)]
-      (let [key* (next key*)]
-        (if (identical? key first)
-            (conj (next coll) (apply update-in+ (first coll) key* f arg*))
-            (assoc coll key (apply update-in+ (get coll key) key* f arg*))))
-      (apply f coll arg*)))
-
 (defn build-invoke-expr
-  [rator rand]
-  {:args
-   (list rand),
+  [rator rand*]
+  {:args rand*,
    :op :invoke,
    :protocol-on nil,
    :is-protocol false,
-   :fexpr
-   rator,
+   :fexpr rator,
    :is-direct false,
    :env
    ;; NB: consider flowing useful source information here
    {:source "unknown",
-    :column nil,
-    :line nil,
+    :column -1,
+    :line -1,
     :locals {},
     :ns {:name 'clojure.tools.cps}},
    :site-index -1,
    :tag nil})
 
-(defn unsupported!
-  [expr-str]
-  (throw (Exception. (str expr-str " expressions not supported yet"))))
+(defn build-continuation-expr
+  [arg body]
+  ;; NB: missing lots of source information here
+  {:op :fn-expr,
+   :env {:line -1, :locals {}, :ns {:name 'clojure.tools.cps}},
+   :methods
+   (list {:op :fn-method,
+          :env {:locals {}, :ns {:name 'clojure.tools.cps}},
+          :body
+          {:op :do,
+           :env
+           {:source "unknown",
+            :column -1,
+            :line -1,
+            :locals {},
+            :ns {:name 'clojure.tools.cps}},
+           :exprs (list body)},
+          :required-params (list arg),
+          :rest-param nil}),
+   :variadic-method nil,
+   :tag nil})
+
+(def empty-continuation
+  (let [x (make-fresh-var 'x)]
+    (build-continuation-expr x x)))
+
+(declare cps-triv cps-srs)
 
 ;; NB: assuming single-body input fn expressions for now
 ;; NB: assuming only required symbol arguments (no rest or arg destructuring)
@@ -82,28 +102,111 @@
         (update-in+ [:methods first :required-params] conj k)
         (update-in+ [:methods first :body :exprs first]
           #(if (trivial-expr? %)
-               (build-invoke-expr k %)
-               (unsupported! "serious expressions in fn bodies"))))))
+               (build-invoke-expr k (list %))
+               (cps-srs % k))))))
     
-(defn cps-expr
+(defn cps-app
+  [expr k]
+  (letfn [(trivit [e]
+            (if (trivial-expr? e)
+                (cps-triv e)
+                (make-fresh-var 's)))]
+    ;; NB: this is a den of inefficient things
+    (loop [rcall* (reverse (conj (:args expr) (:fexpr expr)))
+           rtriv* (map trivit rcall*)
+           out (build-invoke-expr (last rtriv*)
+                 (conj (reverse (butlast rtriv*)) k))]
+      (if (and (nil? (seq rcall*)) (nil? (seq rtriv*)))
+          out
+          (let [[rcall & rcall*] rcall*
+                [rtriv & rtriv*] rtriv*]
+            (if (trivial-expr? rcall)
+                (recur rcall* rtriv* out)
+                (recur rcall* rtriv*
+                  (cps-srs rcall (build-continuation-expr rtriv out)))))))))
+                
+(defn error
+  [& msg]
+  (throw (Exception. (apply str msg))))
+
+(defn unsupported!
+  [expr-str]
+  (error (str expr-str " expressions not supported yet")))
+
+(defn cps-triv
   [expr]
   (case (:op expr)
-    (:number :boolean :constant :string :keyword) expr
-    :def             (unsupported! "def")
+    (:var
+     :local-binding
+     :local-binding-expr
+     :number
+     :boolean
+     :constant
+     :string
+     :keyword)
+    ,expr
     :fn-expr         (cps-fn expr)
+    :def             (unsupported! "def")
     :if              (unsupported! "if")
     :do              (unsupported! "do")
     :let             (unsupported! "let")
-    :invoke          (unsupported! "invoke")
     :recur           (unsupported! "recur")
     :throw           (unsupported! "throw")
     :try             (unsupported! "try")
     :monitor-enter   (unsupported! "monitor-enter")
     :monitor-exit    (unsupported! "monitor-exit")
-    :instance-method (unsupported! "instance methods invokes")
+    :instance-method (unsupported! "instance method invokes")
     :new             (unsupported! "new")
     :static-method   (unsupported! "static-method invokes")
-    (throw (Exception. (str "unexpected expression " expr)))))
+    (error "unexpected trivial expression " (emit/emit-form expr))))
+
+(defn cps-srs
+  [expr k]
+  (case (:op expr)
+    :invoke          (cps-app expr k)
+    :def             (unsupported! "def")
+    :if              (unsupported! "if")
+    :do              (unsupported! "do")
+    :let             (unsupported! "let")
+    :recur           (unsupported! "recur")
+    :throw           (unsupported! "throw")
+    :try             (unsupported! "try")
+    :monitor-enter   (unsupported! "monitor-enter")
+    :monitor-exit    (unsupported! "monitor-exit")
+    :instance-method (unsupported! "instance method invokes")
+    :new             (unsupported! "new")
+    :static-method   (unsupported! "static-method invokes")
+    (error "unexpected serious expression " (emit/emit-form expr))))
+
+(defn cps-expr
+  [expr]
+  (case (:op expr)
+    (:var
+     :number
+     :boolean
+     :constant
+     :string
+     :keyword
+     :fn-expr)
+    ,(cps-triv expr)
+    :invoke 
+    ,(cps-app expr empty-continuation)
+    (:def
+     :if
+     :do
+     :let
+     :recur
+     :throw
+     :try
+     :monitor-enter
+     :monitor-exit
+     :instance-method
+     :new
+     :static-method)
+    ,(if (trivial-expr? expr)
+         (cps-triv expr)
+         (cps-srs expr (make-fresh-var 'k)))
+    (error (str "unexpected expression " expr))))
 
 (defmacro cps
   [expr]
